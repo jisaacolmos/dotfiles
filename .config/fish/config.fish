94bd2ed9bdc28221483a85b18e646297ca88249f
Jorge Olmos <jisaac.olmos@ikasle.aeg.eus>
2026-09-23 13:56:36 +0200

    Updated fish conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,7 @@ if status is-interactive
 
     fish_add_path /home/j0lms/.local/bin
     fish_add_path /home/j0lms/Descargas/jdt-language-server-1.9.0-202203031534/bin
+
     # Aliases
 
     alias fb="fzf --ansi --preview 'bat --color=always {}' --preview-window '~3' --preview-window=right:60%"
@@ -44,8 +45,79 @@ if status is-interactive
     alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
     alias lS='eza -1'
     alias lt='eza -a --tree --level=2'
+    alias g='git'
+    alias gst='git status'
+    alias gd='git diff'
+    alias gdc='git diff --cached'
+    alias gl='git pull'
+    alias gup='git pull --rebase'
+    alias gp='git push'
+    alias gd='git diff'
+    alias gc='git commit -v'
+    alias gc!='git commit -v --amend'
+    alias gca='git commit -v -a'
+    alias gca!='git commit -v -a --amend'
+    alias gcmsg='git commit -m'
+    alias gco='git checkout'
+    alias gcm='git checkout master'
+    alias gr='git remote'
+    alias grv='git remote -v'
+    alias grmv='git remote rename'
+    alias grrm='git remote remove'
+    alias grset='git remote set-url'
+    alias grup='git remote update'
+    alias grbi='git rebase -i'
+    alias grbc='git rebase --continue'
+    alias grba='git rebase --abort'
+    alias gb='git branch'
+    alias gba='git branch -a'
+    alias gcount='git shortlog -sn'
+    alias gcl='git config --list'
+    alias gcp='git cherry-pick'
+    alias glg='git log --stat --max-count=10'
+    alias glgg='git log --graph --max-count=10'
+    alias glgga='git log --graph --decorate --all'
+    alias glo='git log --oneline'
+    alias gss='git status -s'
+    alias ga='git add'
+    alias gm='git merge'
+    alias grh='git reset HEAD'
+    alias grhh='git reset HEAD --hard'
+    alias gclean='git reset --hard; and git clean -dfx'
+    alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+    alias gpoat='git push origin --all; and git push origin --tags'
+    alias gmt='git mergetool --no-prompt'
+    alias gg='git gui citool'
+    alias gga='git gui citool --amend'
+    alias gk='gitk --all --branches'
+    alias gsts='git stash show --text'
+    alias gsta='git stash'
+    alias gstp='git stash pop'
+    alias gstd='git stash drop'
+    alias grt='cd (git rev-parse --show-toplevel; or echo ".")'
+    alias git-svn-dcommit-push='git svn dcommit; and git push github master:svntrunk'
+    alias gsr='git svn rebase'
+    alias gsd='git svn dcommit'
+    alias ggpull='git pull origin (current_branch)'
+    alias ggpur='git pull --rebase origin (current_branch)'
+    alias ggpush='git push origin (current_branch)'
+    alias ggpnp='git pull origin (current_branch); and git push origin (current_branch)'
 
     # Functions
+
+    function gdv
+        git diff -w $argv | view -
+    end
+
+    function current_branch
+        set ref (git symbolic-ref HEAD 2> /dev/null); or set ref (git rev-parse --short HEAD 2> /dev/null); or return
+        echo $ref | sed -e 's|^refs/heads/||'
+    end
+
+    function current_repository
+        set ref (git symbolic-ref HEAD 2> /dev/null); or set ref (git rev-parse --short HEAD 2> /dev/null); or return
+        echo (git remote -v | cut -d':' -f 2)
+    end
 
     function frb --description "Buscar texto con ripgrep, previsualizar con bat y abrir con Emacs/Editor"
         rg --color=always --line-number --no-heading --smart-case "$argv" |
